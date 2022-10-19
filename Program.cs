fefd866eb3885709560d3e593803ee34a6e861f7
IsaacDexter <d015349l@student.staffs.ac.uk>
2022-10-19 20:46:07 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
